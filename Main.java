a40722d7c38748cfe1a98b62056752a616aa08b2
Kola2132 <krzysztof.krowka@onet.eu>
2022-07-04 19:09:47 +0200

    Edited according to comment

--- Main.java
+++ Main.java
@@ -2,12 +2,7 @@ import java.security.PermissionCollection;
 
 public class Main {
   public static void main(String[] args) {
-    Person myPerson = new Person();
-    myPerson.age="16";
-    myPerson.height="192";
-    myPerson.weight="83";
-    myPerson.name="Krzysiek";
-    myPerson.surname="Krowka";
+    Person myPerson = new Person("Krzysiek","Krowka");
     System.out.println(myPerson.sayHello());
     System.out.println(myPerson.allInformation());
  }
--- Person.java
+++ Person.java
@@ -1,5 +1,10 @@
 public class Person{
-    String age,height,weight,name,surname;
+    String name,surname;
+    int age,height,weight;
+    Person(String tempName,String tempSurname){
+      name=tempName;
+      surname=tempSurname;
+    }
     public String allInformation(){
         return "Name: "+name+"\nSurname: "+surname+"\nAge: "+age+"\nWeight: "+weight+"\nHeight: "+height;     
     }
